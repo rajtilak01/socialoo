b07979d9c5188794751ba5940a3bde6ffb0f8b09
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2024-02-14 23:30:02 +0530

    auth slice

--- src/store/store.js
+++ src/store/store.js
@@ -1,3 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice";
 
-const store = configureStore({})
+const store = configureStore({
+    reducer: {
+        auth: authSlice,
+    }
+})
+
+export default store;
