5763a356cc4d50773a72760f5f8e9bd42bcbab9b
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2024-02-15 14:07:42 +0530

    updating routes in app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx'
+import AllPosts from './pages/AllPosts.jsx'
+import AuthLayout from './components/AuthLayout.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {
+        path: '/',
+        element: <Home/>
+      },
+      {
+        path: '/login',
+        element: (
+          <AuthLayout authentication={false}>
+            <Login/>
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/signup',
+        element: (
+          <AuthLayout authentication={false}>
+            <Signup/>
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/all-posts',
+        element: (
+          <AuthLayout authentication={true}>
+            <AllPosts/>
+          </AuthLayout>
+        )
+      }
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
