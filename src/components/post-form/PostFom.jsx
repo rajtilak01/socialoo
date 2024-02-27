63ea77b3ef3a49b3fe95ea2625b16db9ba267298
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2024-02-27 10:05:23 +0530

    post form setup

---
+++ src/components/post-form/PostFom.jsx
@@ -0,0 +1,120 @@
+import { useCallback } from "react";
+import {useForm} from "react-hook-form"
+import Button from '../Button'
+import Input from '../Input'
+import RTE from "../RTE";
+import Select from '../Select'
+import appwriteService from '../../appwrite/config'
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+export default function PostForm({post}){
+    const {register,handleSubmit,watch,setValue,control,getValues} = useForm({
+        title: post?.title || "",
+        slug: post?.slug || "",
+        content: post?.content || "",
+        status: post?.status || "acitve",
+    })
+
+    const navigate = useNavigate();
+    const userData = useSelector((state)=> state.auth.userData);
+
+    const submit = async(data) => {
+        if(post) {
+            const file = data.image[0] ? await appwriteService.uploadFile(data.iamge[0]) : null;
+
+            if(file) {
+                await appwriteService.deleteFile(post.featuredImage)
+            }
+
+            const dbPost = await appwriteService.updatePost(post.$id, {
+                ...data,
+                featuredImage: file? file.$id : undefined
+
+            })
+
+            if(dbPost) {
+                navigate(`/post/${dbPost.$id}`)
+            }
+        } else {
+            const file = await appwriteService.uploadFile(data.image[0]);
+            if(file) {
+                const fileId = file.$id;
+                data.featuredImage = fileId;
+            }
+        }
+    }
+
+    const slugTransform = useCallback((value) => {
+        if(value && typeof value === "string") return value.trim().toLowerCase().replace(/[^a-zA-Z\d\s]+/g,'-')
+        .replace(/\s/g,'-')
+    },[]);
+
+    React.useEffect(()=> {
+        watch((value, {name}) => {
+            if(name === "title"){
+                setValue("slug", slugTransform(value.title), {shouldValidate: true})
+            }
+        })
+    },[watch, slugTransform, setValue])
+
+    return (
+        <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
+            <div className="w-2/3 px-2">
+                <Input 
+                label="Title"
+                placeholder="Title"
+                className="mb-4"
+                {...register("title", {required: true})}
+                />
+                <Input 
+                label= "Slug:"
+                placeholder= "Slug"
+                className="mb-4"
+                {...register('slug', {required: true})}
+                onInput={(e) => {
+                    setValue("slug", slugTransform(e.currentTarget.value), {shouldValidate: true});
+                }}
+                />
+                <RTE
+                label="Content"
+                name="content"
+                control={control}
+                defaultValue={getValues("content")}
+                // onChange={(value) => setValue("content", value, {shouldValidate: true})}
+                />
+            </div>
+            <div className="w-1/3 px-2">
+                <Input
+                label="Featured Image"
+                type="file"
+                className="mb-4"
+                accept="image/png, image/jpg, image/jpeg"
+                {...register("image", {required: !post})}
+                />
+
+                {post && (
+                    <div className="w-full mb-4">
+                        <img src={appwriteService.getFilePreview(post.featuredImage)} alt={post.title}
+                        className="rouded-lg"
+                        />
+                    </div>
+                )}
+
+                <Select
+                options={["active", "inactive"]}
+                label="Status"
+                className="mb-4"
+                {...register("status", {required: true})}
+                />
+                <Button
+                type="submit"
+                className="w-full mt-4"
+                bgColor={post ? "bg-green-500" : undefined}
+                >
+                    {post ? "Update" : "Create"}
+                </Button>
+            </div>
+        </form>
+    )
+}
